0cf0abfd854cdffad6a9e0f4f3ba42d52ef26f86
Sterling Davis <sterlingdayvis@gmail.com>
2022-12-22 11:52:11 -0800

    Add files via upload
    trained model multiple x's + loaded in our model using pickle

--- tensorEnv/tensorEnn.py
+++ tensorEnv/tensorEnn.py
@@ -1,10 +1,17 @@
 # Import required libraries. 
 
-import pandas as pd
 import numpy as np
-import sklearn
+import pandas as pd
 from sklearn import linear_model
+import sklearn
 from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
+
+# Specify our graph plotting style. 
+
+style.use("ggplot")
 
 # Loading in data 
 
@@ -24,31 +31,57 @@ data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
 predict = "G3"
 
-X = np.array(data.drop([predict], 1)) # Features
+x = np.array(data.drop([predict], 1)) # Features
 y = np.array(data[predict]) # Label
 
 # Now we will split our data into testing + training sets. 90% of our data will be used to train and 10% to test. 
 
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-# Define model that we will be using. 
+# Train model multiple times for best accuracy
 
-linear = linear_model.LinearRegression()
+best = 0
+for _ in range(20):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-# Train and score our model utilizing arrays
+    linear = linear_model.LinearRegression()
 
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test) # acc (this is our accuracy score)
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print("Accuracy: " + str(acc))
 
-# This will display the constants used to generate our line for linear regression:
+    if acc > best:
+        best = acc
+        with open("studentgrades.pickle", "wb") as f: #"wb" will open the file in binary form for writing 
+            pickle.dump(linear, f)
 
-print('Coefficient: \n', linear.coef_) # slope value
-print('Intercept: \n', linear.intercept_) # intercept
 
-# Predicting grade scores on specific students 
+# Loading in our model using .pickle. "rb" will open the file in binary form for reading 
 
-predictions = linear.predict(x_test) # Gets a list of all predictions
+pickle_in = open("studentgrades.pickle", "rb")
+linear = pickle.load(pickle_in)
 
-for x in range(len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
+
+print("-------------------------")
+print('Coefficient: \n', linear.coef_)
+print('Intercept: \n', linear.intercept_)
+print("-------------------------")
+
+predicted= linear.predict(x_test)
+for x in range(len(predicted)):
+    print(predicted[x], x_test[x], y_test[x])
+
+
+# Drawing and plotting model
+plot = "failures"
+plt.scatter(data[plot], data["G3"])
+plt.legend(loc=4)
+plt.xlabel(plot)
+plt.ylabel("Final Grade")
+plt.show()
+
+
+
+
+
